f5481fc12da02f9bbc928cc1eeca37e0688b2eb3
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-10-10 20:00:10 +0700

    feat: update ESLint configuration and refine build script in package.json

--- frontend/2FA-MERN/eslint.config.js
+++ frontend/2FA-MERN/eslint.config.js
@@ -1,9 +1,9 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +14,35 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      ...tseslint.configs.stylisticTypeChecked
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn'],
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      'indent': ['warn', 2],
+      'semi': [1, 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
+    }
   },
 ])
