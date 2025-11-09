33bf06a7e517ad5514b3e5d45f07b763d81568af
Ngô Việt Hoàng <162393499+Ngo-Viet-Hoang1@users.noreply.github.com>
2025-11-09 12:39:26 +0700

    fix(eslint): remove unused 'node' import from globals

--- frontend/2FA-MERN/eslint.config.js
+++ frontend/2FA-MERN/eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import globals, { node } from 'globals'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default defineConfig([
